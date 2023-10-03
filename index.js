f5086c74450d3f9442a9fc5b0fbd307e9e98dd23
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-04 02:09:35 +0530

    feat - data received from backend

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// app.use("/",router);
+app.use("/",router);
 
 app.listen(process.env.PORT,()=>{
     console.log(`server listening...`)
--- routes/route.js
+++ routes/route.js
@@ -6,3 +6,4 @@ const router = express.Router();
 router.post("/addRemainder",addRemainder);
 
 export default router;
+ 
